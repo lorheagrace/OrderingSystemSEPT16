7bca55fabfe96cc5c40ca61be32d06221f76b225
lorheagrace <lorheagrace.villena@gsfe.tupcavite.edu.ph>
2025-09-17 10:58:51 +0800

    Update urls.py

--- OrderingSystem/OrderingSystem/urls.py
+++ OrderingSystem/OrderingSystem/urls.py
@@ -50,7 +50,7 @@ else:
         re_path(r'^media/(?P<path>.*)$', serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
-        re_path(r'^static/(?P<path>.*)$, serve, {
+        re_path(r'^static/(?P<path>.*), serve, {
             'document_root': settings.STATIC_ROOT,
         }),
     ]
