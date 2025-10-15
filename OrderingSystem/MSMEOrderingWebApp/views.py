ec9395c53e0a8268d3d180895ed90e37044665f3
lorheagrace <lorheagrace.villena@gsfe.tupcavite.edu.ph>
2025-10-15 10:00:17 +0800

    Update views.py

--- OrderingSystem/MSMEOrderingWebApp/views.py
+++ OrderingSystem/MSMEOrderingWebApp/views.py
@@ -4152,7 +4152,7 @@ def cashier_dashboard(request):
 
     total_preparing = count_unique_orders(
         Checkout.objects.filter(
-            status__in=["accepted", "Preparing", "Packed", "Ready for Pickup", "Ready for Delivery"]
+            status__in=["accepted", "Preparing", "Packed", "Out for Delivery", "Ready for Pickup", "delivered"]
         )
     )
 
